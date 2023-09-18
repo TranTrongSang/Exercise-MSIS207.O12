ef900d728065fc079c984fe1373b5427cf9ba3ac
Sang <20521834@gm.uit.edu.vn>
2023-09-18 22:56:26 +0700

    delete some comments
    Which is unnecessary...

--- Week1/ex2/Video4/index.js
+++ Week1/ex2/Video4/index.js
@@ -25,28 +25,7 @@ require('./routes')(app);
 
 let server = require('http').createServer(app);
 
-// app.get('/', function(req, res) 
-// {
-// 	res.send('Hello World');
-// });
-
 server.listen(3000, function()
 {
 	console.log("Server app is running at port 3000!");
 });
-
-// const http = require("http");
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res)=>
-// {
-// 	res.statusCode = 200;
-// 	res.setHeader('Content-Type', 'text/plain');
-// 	res.end('Helo World');
-// });
-
-// server.listen(port, hostname, ()=>
-// {
-// 	console.log('server running at http://' + hostname + ':' + port + '/');
-// });
